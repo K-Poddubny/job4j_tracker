c72ecaf4fab43fc80262c7f64c6145f53659c917
Kpodd <k.poddubny91@gmail.com>
2022-05-09 17:33:12 +0300

    Вызов метода объекта [#166085]

---
+++ src/main/java/ru/job4/oop/Student.java
@@ -0,0 +1,12 @@
+package ru.job4.oop;
+
+public class Student {
+    public void music() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student kostya = new Student();
+        kostya.music();
+    }
+}
