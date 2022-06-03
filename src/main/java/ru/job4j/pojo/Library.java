3734ca44a3b859eebb28eff1bf80ed01afcd5c29
Kpodd <k.poddubny91@gmail.com>
2022-06-03 17:44:43 +0300

    Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,34 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Золотая рыбка", 19);
+        Book book2 = new Book("Колобок", 15);
+        Book book3 = new Book("Clean code", 105);
+        Book book4 = new Book("Алладин", 3);
+        Book[] allBook = new Book[4];
+        allBook[0] = book1;
+        allBook[1] = book2;
+        allBook[2] = book3;
+        allBook[3] = book4;
+        for (int index = 0; index < allBook.length; index++) {
+            Book fairyTales = allBook[index];
+            System.out.println(fairyTales.getName() + " - " + fairyTales.getPage());
+        }
+        System.out.println("Меняем местами элементы массива");
+        Book temp = allBook[0];
+        allBook[0] = allBook[3];
+        allBook[3] = temp;
+        for (int index = 0; index < allBook.length; index++) {
+            Book fairyTales = allBook[index];
+            System.out.println(fairyTales.getName() + " - " + fairyTales.getPage());
+        }
+        System.out.println("Выводим на консоль книгу с именем Clean code");
+        for (int index = 0; index < allBook.length; index++) {
+            Book fairyTales = allBook[index];
+            if (fairyTales.getName().equals("Clean code")) {
+                System.out.println(fairyTales.getName() + " - " + fairyTales.getPage());
+            }
+        }
+    }
+}
