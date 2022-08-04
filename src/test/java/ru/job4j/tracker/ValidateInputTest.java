2fe4cbde210ee52e57136bd7aff6d9d403ebac24
Kpodd <k.poddubny91@gmail.com>
2022-08-04 20:18:33 +0300

    Тест на ValidateInput [#311887 #326193]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -9,12 +9,15 @@ public class ValidateInputTest {
 
     @Test
     public void whenInvalidInput() {
+        // given
         Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
+        // when
         int selected = input.askInt("Enter menu:");
+        // then
         assertThat(selected, is(1));
     }
 
@@ -31,17 +34,20 @@ public class ValidateInputTest {
 
     @Test
     public void whenMultipleInput() {
+        // given
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"1", "1", "1"}
+                new String[] {"1", "2", "3"}
         );
         ValidateInput input = new ValidateInput(out, in);
+        // when
         int selected = input.askInt("Enter menu:");
-        input.askInt("Enter menu:");
-        input.askInt("Enter menu:");
+        int selected2 = input.askInt("Enter menu:");
+        int selected3 = input.askInt("Enter menu:");
+        // then
         assertThat(selected, is(1));
-        assertThat(selected, is(1));
-        assertThat(selected, is(1));
+        assertThat(selected2, is(2));
+        assertThat(selected3, is(3));
     }
 
     @Test
