54c359ae051ed11d0b5543c13c02f0955332ae05
Kpodd <k.poddubny91@gmail.com>
2022-08-04 20:35:47 +0300

    Коллекции, ArrayList, List, Обобщения [#10094 #329003]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -6,7 +6,7 @@ public class UsageArrayList {
 
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
-        names.add("Petr");
+        names.add("Kostya");
         names.add("Ivan");
         names.add("Stepan");
         for (String rsl : names) {
