389e49e0fd0182c91ad317fac567777b6d971a77
Kpodd <k.poddubny91@gmail.com>
2022-11-04 19:55:54 +0400

    Функции высшего порядка [#24255]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
@@ -25,5 +27,15 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Vasya");
         assertTrue(persons.isEmpty());
+    }
+
+    @Test
+    public void whenFindByName2() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
 }
